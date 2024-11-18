7920c80ae9c9d766e47f76758ccb9c5ad009c7cb
hunter <huntcsg@gmail.com>
2024-11-17 23:00:03 -0500

    Added role_permission table and basic permissions for Templates

--- src/mc_bench/schema/postgres/auth/__init__.py
+++ src/mc_bench/schema/postgres/auth/__init__.py
@@ -2,6 +2,7 @@ from ._auth_provider import auth_provider
 from ._auth_provider_email_hash import auth_provider_email_hash
 from ._permission import permission
 from ._role import role
+from ._role_permission import role_permission
 from ._user import user
 from ._user_role import user_role
 
@@ -11,5 +12,6 @@ __all__ = [
     "user",
     "permission",
     "role",
+    "role_permission",
     "user_role",
 ]
