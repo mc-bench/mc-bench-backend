d796678a78e823a434bfbdec75ea5193d49bce7a
hunter <huntcsg@gmail.com>
2025-02-17 18:24:50 -0500

    wip revert

--- src/mc_bench/apps/worker/tasks/elo_calculation.py
+++ src/mc_bench/apps/worker/tasks/elo_calculation.py
@@ -16,6 +16,26 @@ def elo_calculation():
         db.execute(sqlalchemy.text("SELECT 1"))
         logger.info("Elo calculation completed")
 
+        # get earliest unprocesseed rows (lock for update)
+        latest_timestamp = get_latest_unprocessed_rows(db)  # <- will look at index
+        rows = get_unprocessed_rows(db, batch_size=1000)    # <- will look at index
+
+        if rows:
+            leaderboard = get_leaderboard_rows(db) # lock for update
+
+            for row in rows:
+                update_leaderboard(db, leaderboard, row)
+
+                # get current leaderboard scores (probably lock for writes)
+                # DO IN PYTHON
+                # Update leaderboard
+                # Update with processed flag
+            write_leader_leaderboard(db, leaderboard)
+            mark_rows_as_processed(db, rows)
+    
+    # implicit commit
+
+
     redis = get_redis_client(RedisDatabase.COMPARISON)
     try:
         logger.info("Deleting elo calculation in progress key")
